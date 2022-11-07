f4ef8940df239c94d795a4d2d2e6ffecfd4937d3
Rastenbe <stenbra7@gmail.com>
2022-11-07 19:31:10 +0200

    100% branch covarage for statistics.py

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,39 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+    
+    def test_players_set(self):
+        a  = self.statistics._players
+        b=PlayerReaderStub().get_players()
+        self.assertEqual(a[0].name,b[0].name)
+    
+    def test_search_exist(self):
+        a=self.statistics.search("Semenko")
+        self.assertAlmostEqual(a.name,"Semenko")
+    
+    def test_search_not_found(self):
+        self.assertIsNone(self.statistics.search("asdad"))
+
+    def test_team(self):
+        self.assertAlmostEqual(self.statistics.team("PIT")[0].name,"Lemieux")
+    
+    def test_top(self):
+        self.assertAlmostEqual(self.statistics.top(1)[0].name,"Gretzky")
+    
