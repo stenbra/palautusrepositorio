ed730307c80087ef702bfe0a644eaaa89f7d8719
Rastenbe <stenbra7@gmail.com>
2022-11-28 20:30:27 +0200

    test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosolio toimii

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -61,3 +61,10 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.tuotteen_nimi(),"Maito")
         self.assertEqual(ostos.hinta(),3)
 
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        pekoni = Tuote("Pekoni", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(pekoni)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset),2)
