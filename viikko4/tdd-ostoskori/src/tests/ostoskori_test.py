fbb58787d736c676440cfbed37aa6ca8f79f1245
Rastenbe <stenbra7@gmail.com>
2022-11-28 20:08:05 +0200

    testi kahden eri tuotteen lisämiseen määrä toimi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(),3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavara(self):
+        maito = Tuote("Maito", 3)
+        pekoni = Tuote("Pekoni", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(pekoni)
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
